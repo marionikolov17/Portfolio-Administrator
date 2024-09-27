1a9e72460803ef73ac074dda2dec8ec2b370e606
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-27 12:29:48 +0300

    Create mobile navigation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import DesktopNavigation from "../../features/layout/components/DesktopNavigation/DesktopNavigation";
+import MobileNavigation from "../../features/layout/components/MobileNavigation/MobileNavigation";
 
 export default function Home() {
     return (
         <>
             <main className="w-full bg-primary-950 min-h-full absolute flex font-poppins">
                 <DesktopNavigation />
+                <MobileNavigation />
             </main>
         </>
     )
