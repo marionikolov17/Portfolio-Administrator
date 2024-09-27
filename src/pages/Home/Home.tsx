9f8b06339fa9bdbfa32ef3c96c8cdea73dea7249
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-27 12:56:29 +0300

    Create css class to hide scrollbar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
     return (
         <>
-            <section></section>
+            <section className="no-scrollbar">
+
+            </section>
         </>
     )
 }
