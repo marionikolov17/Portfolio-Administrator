005520c06fc48702153d9ceadd231b19565008a5
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-30 23:21:56 +0300

    Create notifications action form

--- src/features/notifications/components/NotificationsActionForm/NotificationsActionForm.tsx
+++ src/features/notifications/components/NotificationsActionForm/NotificationsActionForm.tsx
@@ -3,9 +3,9 @@ import { IoCalendarClearOutline } from "react-icons/io5";
 export default function NotificationsActionForm() {
     return (
         <>
-            <div className="w-full px-4 py-3 flex flex-wrap justify-center">
-                <p className="text-white my-auto">150 Notifications</p>
-                <div className="ms-4 flex items-center bg-transparent py-2 px-4 border border-primary-800 rounded-lg">
+            <div className="w-full px-4 py-3 flex flex-wrap justify-center gap-x-4">
+                <p className="text-white my-auto hidden sm:inline-block">150 Notifications</p>
+                <div className="flex items-center bg-transparent py-2 px-4 border border-primary-800 rounded-lg">
                     <IoCalendarClearOutline className="me-2 text-brand-600 text-lg"/>
                     <select name="" id="" className="outline-none bg-transparent text-sm text-white">
                         <option value="" className="text-black">Last 7 days</option>
@@ -13,7 +13,7 @@ export default function NotificationsActionForm() {
                         <option value="" className="text-black">All time</option>
                     </select>
                 </div>
-                <div className="ms-4 flex items-center bg-transparent py-2 px-4 border border-primary-800 rounded-lg">
+                <div className="flex items-center bg-transparent py-2 px-4 border border-primary-800 rounded-lg">
                     <select name="" id="" className="outline-none bg-transparent text-sm text-white">
                         <option value="" className="text-black">Visits</option>
                         <option value="" className="text-black">Clicks</option>
--- src/pages/Notifications/Notifications.tsx
+++ src/pages/Notifications/Notifications.tsx
@@ -6,7 +6,7 @@ export default function Notifications() {
             <section className="w-full flex justify-center py-10 px-4 no-scrollbar">
                 <div className="w-full 2xl:w-[70%] h-max bg-primary-900 rounded-lg shadow overflow-hidden">
                     <div className="flex px-6 py-3 bg-primary-800">
-                        <h1 className="text-white text-2xl">Notifications</h1>
+                        <h1 className="text-white text-lg font-bold sm:text-2xl">Notifications</h1>
                     </div>
                     {/* Searching, filtering and sorting */}
                     <NotificationsActionForm />
