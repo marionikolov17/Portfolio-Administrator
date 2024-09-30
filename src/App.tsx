3161c0ffa90ae8bf3693d379f6f5ba63d49bd702
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-30 18:48:07 +0300

    Create route for notifications page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import MainLayout from "./layouts/MainLayout/MainLayout";
+import Notifications from "./pages/Notifications/Notifications";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index Component={Home}/>
+          <Route index Component={Notifications}/>
         </Route>
       </Routes>
     </>
