218a97eb5a398d850984701fdabd2837da697deb
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-30 23:01:31 +0300

    Import notifications action form in notifications page

--- src/pages/Notifications/Notifications.tsx
+++ src/pages/Notifications/Notifications.tsx
@@ -1,5 +1,17 @@
+import NotificationsActionForm from "../../features/notifications/components/NotificationsActionForm/NotificationsActionForm";
+
 export default function Notifications() {
     return (
-        <></>
+        <>
+            <section className="w-full flex justify-center py-10 px-4 no-scrollbar">
+                <div className="w-full 2xl:w-[70%] h-max bg-primary-900 rounded-lg shadow overflow-hidden">
+                    <div className="flex px-6 py-3 bg-primary-800">
+                        <h1 className="text-white text-2xl">Notifications</h1>
+                    </div>
+                    {/* Searching, filtering and sorting */}
+                    <NotificationsActionForm />
+                </div>
+            </section>
+        </>
     )
 }
