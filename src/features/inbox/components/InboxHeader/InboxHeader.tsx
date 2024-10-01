997e994118236a595d6fabe6df803bf83a1c2f25
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-01 22:15:20 +0300

    Refactor inbox page header

---
+++ src/features/inbox/components/InboxHeader/InboxHeader.tsx
@@ -0,0 +1,20 @@
+import { IoSearchOutline } from "react-icons/io5";
+
+export default function InboxHeader() {
+  return (
+    <>
+      <header className="flex flex-wrap items-center justify-center gap-x-10 gap-y-3 py-1 px-1">
+        <h1 className="text-white font-bold text-2xl">Inbox</h1>
+        {/* Search bar */}
+        <div className="overflow-hidden flex items-center rounded-lg shadow border border-primary-800 py-2 px-4 w-full sm:w-96 hover:ring-2 hover:ring-blue-900 transition duration-300">
+          <IoSearchOutline className="text-brand-600 text-lg" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="ms-2 bg-transparent outline-none text-white grow"
+          />
+        </div>
+      </header>
+    </>
+  );
+}
--- src/pages/Inbox/Inbox.tsx
+++ src/pages/Inbox/Inbox.tsx
@@ -1,22 +1,11 @@
-import { IoSearchOutline } from "react-icons/io5";
+import InboxHeader from "../../features/inbox/components/InboxHeader/InboxHeader";
 
 export default function Inbox() {
   return (
     <>
       <section className="w-full flex justify-center py-10 px-4 no-scrollbar">
         <div className="w-full 2xl:w-[70%] h-max overflow-x-hidden">
-          <header className="flex flex-wrap items-center justify-center gap-x-10 gap-y-3 py-1 px-1">
-            <h1 className="text-white font-bold text-2xl">Inbox</h1>
-            {/* Search bar */}
-            <div className="overflow-hidden flex items-center rounded-lg shadow border border-primary-800 py-2 px-4 w-full sm:w-96 hover:ring-2 hover:ring-blue-900 transition duration-300">
-                <IoSearchOutline className="text-brand-600 text-lg"/>
-                <input 
-                    type="text" 
-                    placeholder="Search"
-                    className="ms-2 bg-transparent outline-none text-white grow"
-                />
-            </div>
-          </header>
+          <InboxHeader />
         </div>
       </section>
     </>
