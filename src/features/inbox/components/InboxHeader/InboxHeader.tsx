ca291ece6c7a019d6450777760baf7c0540bb56f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-01 22:25:13 +0300

    Update inbox header layout

--- src/features/inbox/components/InboxHeader/InboxHeader.tsx
+++ src/features/inbox/components/InboxHeader/InboxHeader.tsx
@@ -4,15 +4,19 @@ export default function InboxHeader() {
   return (
     <>
       <header className="flex flex-wrap items-center justify-center gap-x-10 gap-y-3 py-1 px-1">
-        <h1 className="text-white font-bold text-2xl">Inbox</h1>
+        <div className="grow flex justify-start">
+            <h1 className="text-white font-bold text-2xl">Inbox</h1>
+        </div>
         {/* Search bar */}
-        <div className="overflow-hidden flex items-center rounded-lg shadow border border-primary-800 py-2 px-4 w-full sm:w-96 hover:ring-2 hover:ring-blue-900 transition duration-300">
-          <IoSearchOutline className="text-brand-600 text-lg" />
-          <input
-            type="text"
-            placeholder="Search"
-            className="ms-2 bg-transparent outline-none text-white grow"
-          />
+        <div className="grow flex justify-end">
+            <div className="overflow-hidden flex items-center rounded-lg shadow border border-primary-800 py-2 px-4 w-full sm:w-96 hover:ring-2 hover:ring-blue-900 transition duration-300">
+            <IoSearchOutline className="text-brand-600 text-lg" />
+            <input
+                type="text"
+                placeholder="Search"
+                className="ms-2 bg-transparent outline-none text-white grow"
+            />
+            </div>
         </div>
       </header>
     </>
--- src/pages/Inbox/Inbox.tsx
+++ src/pages/Inbox/Inbox.tsx
@@ -6,6 +6,10 @@ export default function Inbox() {
       <section className="w-full flex justify-center py-10 px-4 no-scrollbar">
         <div className="w-full 2xl:w-[70%] h-max overflow-x-hidden">
           <InboxHeader />
+          {/* Messages container */}
+          <div className="w-full mt-8 bg-primary-900 rounded-lg shadow overflow-hidden h-max">
+
+          </div>
         </div>
       </section>
     </>
