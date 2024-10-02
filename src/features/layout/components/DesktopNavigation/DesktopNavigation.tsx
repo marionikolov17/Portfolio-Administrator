69526eb61b57eceb137f2130c5fcfe8c9f4954b0
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-02 16:37:42 +0300

    Make desktop navigation links change on page change

--- src/features/layout/components/DesktopNavigation/DesktopNavigation.tsx
+++ src/features/layout/components/DesktopNavigation/DesktopNavigation.tsx
@@ -1,6 +1,9 @@
+import { useLocation } from "react-router-dom";
 import NavigationButton from "../NavigationButton/NavigationButton";
 
 export default function DesktopNavigation() {
+    const location = useLocation();
+
     return (
         <>
             <nav className="hidden sm:block w-20 lg:w-[280px] shrink-0 sticky h-screen top-0 z-50 bg-primary-900">
@@ -9,9 +12,9 @@ export default function DesktopNavigation() {
                 </div>
                 {/* Navigation Buttons */}
                 <div className="w-full flex flex-col items-center px-2 lg:px-6">
-                    <NavigationButton to="/" isActive={true} title="Home" icon="home" />
-                    <NavigationButton to="/notifications" isActive={false} title="Notifications" icon="notifications" />
-                    <NavigationButton to="/inbox" isActive={false} title="Inbox" icon="inbox" />
+                    <NavigationButton to="/" isActive={location.pathname === "/"} title="Home" icon="home" />
+                    <NavigationButton to="/notifications" isActive={location.pathname === "/notifications"} title="Notifications" icon="notifications" />
+                    <NavigationButton to="/inbox" isActive={location.pathname.startsWith("/inbox")} title="Inbox" icon="inbox" />
                     <NavigationButton to="/" isActive={false} title="Projects" icon="projects" />
                     <NavigationButton to="/" isActive={false} title="Certificates" icon="certificates" />
                 </div>
