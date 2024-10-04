040a590d475ff913700269aaadf33951351c94fc
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-04 17:30:20 +0300

    Create method for fetching inbox messages

--- src/entities/messages/services/message.service.ts
+++ src/entities/messages/services/message.service.ts
@@ -1,5 +1,11 @@
-import {  } from "appwrite"
+import { databases } from "../../../lib/appwrite"
+import { DATABASE_ID, INBOX_COLLECTION_ID } from "../../../shared/constants/database.constant"
 
 export const getMessages = async () => {
+    const response = await databases.listDocuments(
+        DATABASE_ID,
+        INBOX_COLLECTION_ID
+    );
 
+    return response;
 }
