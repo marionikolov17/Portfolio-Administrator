d56e065574f07e1cb51a0b60ea418493d9679c4f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-05 14:34:33 +0300

    Handle filtering notifications

--- src/features/notifications/components/NotificationsActionForm/NotificationsActionForm.tsx
+++ src/features/notifications/components/NotificationsActionForm/NotificationsActionForm.tsx
@@ -1,24 +1,26 @@
 import { IoCalendarClearOutline } from "react-icons/io5";
 
 interface CurrentProps {
-    totalNotifications: number
+    totalNotifications: number,
+    handleSelectTime: (e: React.ChangeEvent<HTMLSelectElement>) => void,
+    handleSelectType: (e: React.ChangeEvent<HTMLSelectElement>) => void
 }
 
-export default function NotificationsActionForm({ totalNotifications }: CurrentProps) {
+export default function NotificationsActionForm({ totalNotifications, handleSelectTime, handleSelectType }: CurrentProps) {
     return (
         <>
             <div className="w-full px-4 py-3 flex flex-wrap justify-center gap-x-4">
                 <p className="text-white my-auto hidden sm:inline-block">{totalNotifications} Notifications</p>
                 <div className="flex items-center bg-transparent py-2 px-4 border border-primary-800 rounded-lg">
                     <IoCalendarClearOutline className="me-2 text-brand-600 text-lg"/>
-                    <select name="" id="" className="outline-none bg-transparent text-sm text-white">
+                    <select onChange={handleSelectTime} name="" id="" className="outline-none bg-transparent text-sm text-white">
                         <option value="last-7" className="text-black">Last 7 days</option>
                         <option value="last-30" className="text-black">Last 30 days</option>
                         <option value="all" className="text-black">All time</option>
                     </select>
                 </div>
                 <div className="flex items-center bg-transparent py-2 px-4 border border-primary-800 rounded-lg">
-                    <select name="" id="" className="outline-none bg-transparent text-sm text-white">
+                    <select onChange={handleSelectType} name="" id="" className="outline-none bg-transparent text-sm text-white">
                         <option value="all" className="text-black">All</option>
                         <option value="visits" className="text-black">Visits</option>
                         <option value="clicks" className="text-black">Clicks</option>
--- src/pages/Notifications/Notifications.tsx
+++ src/pages/Notifications/Notifications.tsx
@@ -1,16 +1,32 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import NotificationContainer from "../../features/notifications/components/NotificationContainer/NotificationContainer";
 import NotificationsActionForm from "../../features/notifications/components/NotificationsActionForm/NotificationsActionForm";
 import { getNotifications } from "../../entities/notifications/services/notification.service";
 import Loader from "../../shared/components/Loader/Loader";
+import { useState } from "react";
 
 export default function Notifications() {
+    const [selectedTime, setSelectedTime] = useState("last-7");
+    const [selectedType, setSelectedType] = useState("all");
+
     const { isPending, isError, data, error } = useQuery({
-        queryKey: ["notifications"],
-        queryFn: getNotifications
+        queryKey: ["notifications", selectedTime, selectedType],
+        queryFn: () => getNotifications(selectedTime, selectedType)
     });
 
+    const queryClient = useQueryClient();
+
     const notifications = data?.documents;
+
+    const handleSelectTime = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        queryClient.invalidateQueries({ queryKey: ['inbox', selectedTime, selectedType] });
+        setSelectedTime(e.target.value)
+    }
+
+    const handleSelectType = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        queryClient.invalidateQueries({ queryKey: ['inbox', selectedTime, selectedType] });
+        setSelectedType(e.target.value)
+    }
 
     return (
         <>
@@ -20,7 +36,7 @@ export default function Notifications() {
                         <h1 className="text-white text-lg font-bold sm:text-2xl">Notifications</h1>
                     </div>
                     {/* Searching, filtering and sorting */}
-                    <NotificationsActionForm totalNotifications={data?.total as number}/>
+                    <NotificationsActionForm totalNotifications={data?.total as number} handleSelectTime={handleSelectTime} handleSelectType={handleSelectType}/>
                     {isPending &&
                         <div className="w-full flex items-center justify-center my-10">
                             <Loader />
