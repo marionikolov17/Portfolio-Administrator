31f932e6e3ea568bd3c3907f804ef81f5db449c3
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-05 14:24:17 +0300

    Add filtering in get notifications method

--- src/entities/notifications/services/notification.service.ts
+++ src/entities/notifications/services/notification.service.ts
@@ -1,10 +1,46 @@
+import { Query } from "appwrite";
 import { databases } from "../../../lib/appwrite"
 import { DATABASE_ID, NOTIFICATIONS_COLLECTION_ID } from "../../../shared/constants/database.constant"
 
-export const getNotifications = async () => {
+export const getNotifications = async (selectedTime: string, selectedType: string) => {
+    const filterArr: string[] = [];
+
+    // selected time filtering
+    if (selectedTime == "last-7") {
+        const endDate = new Date();
+        const startDate = new Date(endDate.getDate() - 7);
+        filterArr.push(
+            Query.and([
+                Query.greaterThanEqual("$createdAt", startDate.toISOString()),
+                Query.lessThanEqual("$createdAt", endDate.toISOString())
+            ])
+        );
+    } else if (selectedTime == "last-30") {
+        const endDate = new Date();
+        const startDate = new Date(endDate.getDate() - 30);
+        filterArr.push(
+            Query.and([
+                Query.greaterThanEqual("$createdAt", startDate.toISOString()),
+                Query.lessThanEqual("$createdAt", endDate.toISOString())
+            ])
+        );
+    }
+
+    // selected type filtering
+    if (selectedType == "visits") {
+        filterArr.push(
+            Query.equal("type", "visit")
+        );
+    } else if (selectedType == "clicks") {
+        filterArr.push(
+            Query.equal("type", "click")
+        );
+    }
+
     const response = await databases.listDocuments(
         DATABASE_ID,
-        NOTIFICATIONS_COLLECTION_ID
+        NOTIFICATIONS_COLLECTION_ID,
+        [...filterArr]
     )
 
     return response;
