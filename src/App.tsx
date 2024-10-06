8d645761dc9b0476b0006ea36a52933a29f6ac0f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-06 13:25:34 +0300

    Create error alert container

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,23 @@ import Inbox from "./pages/Inbox/Inbox";
 import InboxMessage from "./pages/InboxMessage/InboxMessage";
 import Login from "./pages/Login/Login";
 import { UserProvider } from "./lib/context/user.context";
+import ErrorAlert from "./shared/components/ErrorAlert/ErrorAlert";
 
 function App() {
   return (
     <UserProvider>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index Component={Home}/>
-          <Route path="notifications" Component={Notifications}/>
-          <Route path="inbox" Component={Inbox}/>
-          <Route path="inbox/:messageId" Component={InboxMessage}/>
-        </Route>
-        <Route path="/login" Component={Login}/>
-      </Routes>
+      <>
+        <ErrorAlert />
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index Component={Home}/>
+            <Route path="notifications" Component={Notifications}/>
+            <Route path="inbox" Component={Inbox}/>
+            <Route path="inbox/:messageId" Component={InboxMessage}/>
+          </Route>
+          <Route path="/login" Component={Login}/>
+        </Routes>
+      </>
     </UserProvider>
   );
 }
--- src/shared/components/ErrorAlert/ErrorAlert.tsx
+++ src/shared/components/ErrorAlert/ErrorAlert.tsx
@@ -1,5 +1,18 @@
+import { IoAlertCircleOutline } from "react-icons/io5";
+
 export default function ErrorAlert() {
     return (
-        <></>
+        <>
+            <div className="fixed w-full min-h-screen flex justify-center items-center z-50 error-bg font-poppins">
+                <div className="w-full h-full absolute -z-10"></div>
+                <div className="w-[450px] max-h-max py-8 rounded-lg shadow bg-primary-900 flex flex-col items-center px-4 z-30">
+                    <IoAlertCircleOutline className="text-white mb-4 text-6xl"/>
+                    <p className="text-red-600">A critical error has occurred. Please contact admin!</p>
+                    <button className="mt-4 py-2 flex w-full justify-center text-white border border-white rounded-lg transition duration-300 hover:bg-white hover:text-primary-950">
+                        Done
+                    </button>
+                </div>
+            </div>
+        </>
     )
 }
