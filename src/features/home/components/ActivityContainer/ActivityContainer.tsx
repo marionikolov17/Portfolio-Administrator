dd3eb7da75ad1465d4142afb55a7102241cfd572
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-06 12:27:28 +0300

    Fetch recent notifications on home page

--- src/features/home/components/ActivityContainer/ActivityContainer.tsx
+++ src/features/home/components/ActivityContainer/ActivityContainer.tsx
@@ -1,23 +1,27 @@
 import { IoPersonCircleOutline } from "react-icons/io5";
+import { INotification } from "../../../../entities/notifications/interfaces/notification.interface";
+import { Models } from "appwrite";
+import { FLAG_ICONS } from "../../../../shared/data/flag-icons";
+import moment from "moment";
 
-export default function ActivityContainer() {
+export default function ActivityContainer({ notification }: { notification: INotification | Models.Document }) {
     return (
         <>
             <div className="w-full flex items-center shrink-0 border-t border-t-primary-800 py-2">
-                <IoPersonCircleOutline className="text-4xl text-brand-600"/>
-                <img 
-                    src="https://flagcdn.com/bg.svg" 
+                <IoPersonCircleOutline className="text-4xl text-brand-600 me-2 sm:me-4"/>
+                {notification?.actorCountry !== null && <img 
+                    src={FLAG_ICONS[notification?.actorCountry]} 
                     alt="flag" 
-                    className="ms-2 sm:ms-4 me-1 w-6 h-6 sm:w-8 sm:h-8"
-                />
+                    className="me-1 w-6 h-6 sm:w-8 sm:h-8"
+                />}
                 <p className="text-white font-medium text-sm sm:text-base">
-                    212.22.22.22
+                    {notification?.actorIP !== null ? notification?.actorIP : "Unknown"}
                 </p>
                 <p className="text-white ms-2">
-                    Visited Portfolio
+                    {notification?.type === "visit" ? "Visited portfolio" : notification?.message}
                 </p>
                 <p className="ms-2 text-sm text-primary-700">
-                    2h
+                    {moment(notification?.$createdAt).fromNow()}
                 </p>
             </div>
         </>
--- src/features/home/components/RecentNotificationsBox/RecentNotificationsBox.tsx
+++ src/features/home/components/RecentNotificationsBox/RecentNotificationsBox.tsx
@@ -1,17 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
 import ActivityContainer from "../ActivityContainer/ActivityContainer";
+import { getNotifications } from "../../../../entities/notifications/services/notification.service";
 
 export default function RecentNotificationsBox({ limit }: { limit: number }) {
+    const { isPending, isError, data, error } = useQuery({
+        queryKey: ["notifications", "home", limit],
+        queryFn: () => getNotifications("all", "all", limit)
+    });
+
+    const notifications = data?.documents;
+
     return (
         <>
             <div className="grow shrink-0 h-80 bg-primary-900 rounded-lg shadow px-3 sm:px-10 py-6">
                 <h3 className="text-lg text-white font-bold">Recent Activity</h3>
 
+                {data && data.total > 0 &&
                 <div className="mt-4 border-b border-b-primary-800">
-                    <ActivityContainer />
-                    <ActivityContainer />
-                    <ActivityContainer />
-                    <ActivityContainer />
-                </div>
+                    {
+                        notifications?.map((notification) => (
+                            <ActivityContainer key={notification.$id} notification={notification}/>
+                        ))
+                    }
+                </div>}
+                {data?.total === 0 && <p className="mt-4 text-white">There are no notifications</p>}
             </div>
         </>
     )
