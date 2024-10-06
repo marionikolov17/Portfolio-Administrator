de99ef33a28bfc17427d18701f28fc3c292bcc88
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-06 12:17:13 +0300

    Handle loading and error on home messages box

--- src/features/home/components/NewMessagesBox/NewMessagesBox.tsx
+++ src/features/home/components/NewMessagesBox/NewMessagesBox.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import NewMessageContainer from "../NewMessageContainer/NewMessageContainer";
 import { getMessages } from "../../../../entities/messages/services/message.service";
+import Loader from "../../../../shared/components/Loader/Loader";
 
 export default function NewMessagesBox({ limit }: { limit: number }) {
   const { isPending, isError, data, error } = useQuery({
@@ -15,13 +16,21 @@ export default function NewMessagesBox({ limit }: { limit: number }) {
       <div className="grow shirnk-0 h-80 bg-primary-900 rounded-lg shadow px-3 sm:px-10 py-6">
         <h3 className="text-lg text-white font-bold">New Messages</h3>
 
+        {isPending &&
+          <div className="w-full mt-4 flex justify-center">
+            <Loader />
+          </div>
+        }
+        {isError && <p className="text-red-600 mt-4">{error.message}</p>}
+        {data && data.total > 0 && 
         <div className="mt-4 w-full">
             {
               messages?.map(message => (
                 <NewMessageContainer key={message?.$id} message={message}/>
               ))
             }
-        </div>
+        </div>}
+        {data?.total === 0 && <p className="text-white mt-4">There are no messages</p>}
       </div>
     </>
   );
