acd704e45132f464081dcc5263b88fd2d43baf6d
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-06 12:45:52 +0300

    Implement method for fetching views statistics

--- src/entities/views/services/view.service.ts
+++ src/entities/views/services/view.service.ts
@@ -1,3 +1,38 @@
-export const fetchViewStatistics = (period: string) => {
+import { Query } from "appwrite";
+import { databases } from "../../../lib/appwrite";
+import { DATABASE_ID, VIEWS_COLLECTION_ID } from "../../../shared/constants/database.constant";
+
+export const fetchViewsStatistics = async (period: string) => {
     const queries = [];
+
+    // Selected period filtering
+    if (period === "last-7") {
+        const endDate = new Date();
+        const startDate = new Date(endDate.getDate() - 7);
+        queries.push(
+            Query.and([
+                Query.greaterThanEqual("$createdAt", startDate.toISOString()),
+                Query.lessThanEqual("$createdAt", endDate.toISOString())
+            ])
+        );
+    }
+
+    if (period === "last-30") {
+        const endDate = new Date();
+        const startDate = new Date(endDate.getDate() - 30);
+        queries.push(
+            Query.and([
+                Query.greaterThanEqual("$createdAt", startDate.toISOString()),
+                Query.lessThanEqual("$createdAt", endDate.toISOString())
+            ])
+        );
+    }
+
+    const response = await databases.listDocuments(
+        DATABASE_ID,
+        VIEWS_COLLECTION_ID,
+        queries
+    )
+
+    return response;
 }
