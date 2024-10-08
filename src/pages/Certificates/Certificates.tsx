80d5ee39fad0495009fe8062b43d0fbdf523495c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-08 20:33:42 +0300

    Add links to certificates page on navigation

--- src/pages/Certificates/Certificates.tsx
+++ src/pages/Certificates/Certificates.tsx
@@ -1,5 +1,11 @@
 export default function Certificates() {
     return (
-        <></>
+        <>
+            <section className="w-full flex justify-center py-10 px-4 no-scrollbar">
+                <div className="w-full 2xl:w-[70%] h-max overflow-x-hidden">
+                    
+                </div>
+            </section>
+        </>
     )
 }
