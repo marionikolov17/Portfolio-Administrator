d371611d0d5d93cdb35ddc1d9a71941fc2dfdf88
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-08 21:19:10 +0300

    Finish certificates page

--- src/features/certificates/components/Certificate/Certificate.tsx
+++ src/features/certificates/components/Certificate/Certificate.tsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+import { IoArrowDownOutline, IoArrowUpOutline, IoCreateOutline, IoTrashOutline } from "react-icons/io5";
+
 export default function Certificate() {
-    return (
-        <></>
-    )
+  return (
+    <>
+      <div className="w-full flex flex-wrap items-center overflow-hidden min-h-14 bg-primary-900 rounded-lg shadow">
+        {/* Certificate image */}
+        <div className="shrink-0 w-max h-full flex justify-center overflow-hidden">
+          <img
+            className="object-cover w-20 max-h-14"
+            src="https://cloud.appwrite.io/v1/storage/buckets/670571e3001ab6e74ef8/files/670572110013b202a8cc/view?project=66fe98e400350a2afae6&project=66fe98e400350a2afae6&mode=admin"
+            alt="certificate image"
+          />
+        </div>
+        <div className="grow shrink-0 flex items-center">
+          <h3 className="ms-4 text-white font-bold">Programming Fundam...</h3>
+        </div>
+        {/* Buttons */}
+        <div className="flex items-center justify-center px-4 w-full grow py-4 border-t border-t-primary-800 sm:border-none sm:py-0 sm:grow-0 sm:w-auto">
+          <Link to="/certificates/edit/1"><IoCreateOutline className="text-brand-600 text-2xl cursor-pointer mx-1.5"/></Link>
+          <IoTrashOutline className="text-red-600 text-2xl cursor-pointer mx-1.5"/>
+          <IoArrowUpOutline className="text-white text-2xl cursor-pointer mx-1.5"/>
+          <IoArrowDownOutline className="text-white text-2xl cursor-pointer mx-1.5"/>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/Certificates/Certificates.tsx
+++ src/pages/Certificates/Certificates.tsx
@@ -1,9 +1,22 @@
+import { MdOutlineNoteAdd } from "react-icons/md";
+import { Link } from "react-router-dom";
+import Certificate from "../../features/certificates/components/Certificate/Certificate";
+
 export default function Certificates() {
     return (
         <>
             <section className="w-full flex justify-center py-10 px-4 no-scrollbar">
-                <div className="w-full 2xl:w-[70%] h-max overflow-x-hidden">
-                    
+                <div className="w-full 2xl:w-[70%] h-max overflow-x-hidden py-1 px-1">
+                    <h1 className="text-white font-bold text-2xl">Certificates</h1>
+                    <Link to="/certificates/create" className="mt-4 w-max text-sm flex justify-center items-center px-4 py-2 text-white rounded-lg border border-primary-800 hover:ring-2 hover:ring-blue-900 transition duration-300">
+                        <MdOutlineNoteAdd  className="me-2 text-xl"/>
+                        Add Certificate
+                    </Link>
+                    {/* Fetched certificates here */}
+                    <div className="w-full mt-6 flex flex-col gap-y-4">
+                        <Certificate />
+                        <Certificate />
+                    </div>
                 </div>
             </section>
         </>
