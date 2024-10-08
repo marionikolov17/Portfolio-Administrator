79c31a7a8b9d53557a92af54b92597ac5d55f4bc
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-08 23:45:13 +0300

    Handle create certificate page image upload

--- src/features/certificates/components/ImageVisualizer/ImageVisualizer.tsx
+++ src/features/certificates/components/ImageVisualizer/ImageVisualizer.tsx
@@ -1,5 +1,32 @@
-export default function ImageVisualizer() {
-    return (
-        <></>
-    )
+import { IoTrashOutline } from "react-icons/io5";
+
+export default function ImageVisualizer({
+  image,
+  onImageRemove,
+}: {
+  image: any;
+  onImageRemove: any;
+}) {
+  return (
+    <>
+      <div className="col-span-full rounded-lg border-2 border-dashed border-gray-900/25 mt-2 sm:mt-8">
+        <div
+          className="relative w-full overflow-hidden max-h-[500px]"
+        >
+          <button
+            className="absolute top-0 right-0 p-4 z-30"
+            type="button"
+            onClick={() => onImageRemove()}
+          >
+            <IoTrashOutline className="text-3xl text-red-700" />
+          </button>
+          <img
+            src={image}
+            className={`object-cover`}
+            alt="Uploaded picture"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/CreateCertificate/CreateCertificate.tsx
+++ src/pages/CreateCertificate/CreateCertificate.tsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { BsFiletypePng } from "react-icons/bs";
+import { allowedImageMimeTypes } from "../../shared/constants/allowed-files.constant";
+import ImageVisualizer from "../../features/certificates/components/ImageVisualizer/ImageVisualizer";
 
 export default function CreateCertificate() {
-  const handleDrop = () => {};
+  const [error, setError] = useState<string | null>(null);
+  const [image, setImage] = useState();
 
-  const onImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const { register, handleSubmit, setValue } = useForm();
+
+  const handleDrop = (e: any) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    const file = files[files.length - 1];
+    handleFile(file);
+  };
+
+  const onImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files || [];
+    const file = files[files.length - 1];
+    handleFile(file);
+  };
+
+  const handleFile = (file: File) => {
+    if (!allowedImageMimeTypes.includes(file.type)) {
+      // Show message
+      return setError("Invalid file type. Only images allowed");
+    }
+    // Visualize image
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = (readerEvent) => {
+      setImage(readerEvent.target?.result as any);
+    };
+    setError(null);
+    setValue("imageUri", file);
+  };
+
+  const onImageRemove = () => {
+    setValue("imageUri", undefined);
+    setImage(undefined);
+  };
 
   return (
     <>
@@ -28,7 +67,7 @@ export default function CreateCertificate() {
               />
             </div>
             {/* Certificate Image */}
-            <div className="col-span-full mt-4">
+            {!image && <div className="col-span-full mt-4">
               <label
                 htmlFor="cover-photo"
                 className="block text-sm font-medium leading-6 text-white"
@@ -64,7 +103,15 @@ export default function CreateCertificate() {
                   </p>
                 </div>
               </div>
-            </div>
+            </div>}
+
+            {image &&
+                <ImageVisualizer 
+                    image={image}
+                    onImageRemove={onImageRemove}
+                />
+            }
+
             {/* Certificate Credential URL */}
             <div className="mt-4 w-full">
               <label
@@ -82,7 +129,7 @@ export default function CreateCertificate() {
               />
             </div>
             <button className="mt-6 w-full rounded-lg flex justify-center py-2 text-white bg-brand-600 hover:bg-brand-700">
-                Create
+              Create
             </button>
           </form>
         </div>
