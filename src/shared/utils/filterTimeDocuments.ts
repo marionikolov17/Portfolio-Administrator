56401cc73ba31ce57168f888dc07ee86b1ee626f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-19 23:14:09 +0300

    Create filterTimeDocuments.ts

---
+++ src/shared/utils/filterTimeDocuments.ts
@@ -0,0 +1,17 @@
+import { Models } from "appwrite";
+
+export const filterTimeDocuments = async (
+  miliseconds: number,
+  documents: Models.Document[]
+) => {
+  const endDate = Date.now();
+  const startDate = new Date(endDate - miliseconds).getTime();
+
+  documents = documents.filter((doc) => {
+    const createdAt = new Date(doc.$createdAt);
+    if (createdAt.getTime() > startDate) return true;
+    return false;
+  });
+
+  return { total: documents.length, documents: documents };
+};
