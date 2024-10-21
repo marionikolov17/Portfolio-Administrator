04938ea3b7e19a1d5191c7346352f38dc25937a8
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-21 13:16:52 +0300

    Finish projects page design

--- src/features/projects/components/Project/Project.tsx
+++ src/features/projects/components/Project/Project.tsx
@@ -1,5 +1,51 @@
+import { IoArrowDownOutline, IoArrowUpOutline, IoCreateOutline, IoTrashOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+
 export default function Project() {
-    return (
-        <></>
-    )
+  const handleClickUp = () => {}
+
+  const handleClickDown = () => {}
+
+  const handleDelete = () => {}
+
+  return (
+    <>
+      <div className="w-full flex flex-wrap items-center overflow-hidden min-h-14 bg-primary-900 rounded-lg shadow">
+        {/* Project thumbnail */}
+        <div className="shrink-0 w-max h-full flex justify-center overflow-hidden relative">
+          <img
+            className="object-cover w-20 h-14"
+            src="https://www.marionikolovdev.com/images/administrator/image-7.jpg"
+            alt="Thumbnail"
+          />
+        </div>
+        <div className="grow shrink-0 flex items-center">
+          <h3 className="ms-4 text-white font-bold xl:hidden">
+            Portfolio Administrator
+          </h3>
+          <h3 className="ms-4 text-white font-bold hidden xl:inline-block">
+            Portfolio Administrator
+          </h3>
+        </div>
+        {/* Buttons */}
+        <div className="flex items-center justify-center px-4 w-full grow py-4 border-t border-t-primary-800 sm:border-none sm:py-0 sm:grow-0 sm:w-auto">
+          <Link to="">
+            <IoCreateOutline className="text-brand-600 hover:text-brand-700 text-2xl cursor-pointer mx-1.5" />
+          </Link>
+          <IoTrashOutline
+            className="text-red-600 hover:text-red-700 text-2xl cursor-pointer mx-1.5"
+            onClick={handleDelete}
+          />
+          <IoArrowUpOutline
+            className="text-white hover:text-brand-600 text-2xl cursor-pointer mx-1.5"
+            onClick={handleClickUp}
+          />
+          <IoArrowDownOutline
+            className="text-white hover:text-brand-600 text-2xl cursor-pointer mx-1.5"
+            onClick={handleClickDown}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,5 +1,23 @@
+import { RiStickyNoteAddLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import Project from "../../features/projects/components/Project/Project";
+
 export default function Projects() {
     return (
-        <></>
+        <>
+            <section className="w-full flex justify-center py-10 px-4 no-scrollbar">
+                <div className="w-full 2xl:w-[70%] h-max overflow-x-hidden py-1 px-1">
+                    <h1 className="text-white font-bold text-2xl">Projects</h1>
+                    <Link to="/projects/create" className="mt-4 w-max text-sm flex justify-center items-center px-4 py-2 text-white rounded-lg border border-primary-800 hover:ring-2 hover:ring-blue-900 transition duration-300">
+                        <RiStickyNoteAddLine  className="me-2 text-xl"/>
+                        Add Project
+                    </Link>
+
+                    <div className="w-full mt-6 flex flex-col gap-y-4">
+                        <Project />
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
