d42801b2a2141170cf0dc41ac2bb29d2152738c4
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-11-16 12:14:12 +0200

    Implement adding tech stacks and changing names

--- src/features/createProject/components/TechStack/TechStack.tsx
+++ src/features/createProject/components/TechStack/TechStack.tsx
@@ -11,17 +11,30 @@ interface InputProps {
 }
 
 export default function TechStack({ addedTech, setAddedTech }: InputProps) {
+  const addNewTech = () => {
+    setAddedTech(currentTech => {
+      const newTech = currentTech.slice(0);
+      newTech.push({ name: "", technologies: [] });
+      return newTech;
+    })
+  }
+
   return (
     <>
       {/* Project tech stack */}
       <div className="w-full mt-4">
         <h4 className="font-bold text-white">Tech Stack</h4>
-        <AddedTech />
+        {
+          addedTech.map((tech, index) => (
+            <AddedTech key={index} tech={tech} index={index} setAddedTech={setAddedTech}/>
+          ))
+        }
         {/* Add new field */}
         <div className="w-full flex justify-center mt-2">
           <button
             type="button"
             className="flex items-center text-white py-2 px-4 border border-primary-800 transition duration-300 rounded-lg hover:ring-2 hover:ring-brand-600"
+            onClick={addNewTech}
           >
             <IoAddOutline className="text-xl me-2" />
             Add new field
@@ -32,18 +45,36 @@ export default function TechStack({ addedTech, setAddedTech }: InputProps) {
   );
 }
 
-function AddedTech() {
+function AddedTech({ tech, index, setAddedTech }: { tech: Tech, index: number, setAddedTech: Dispatch<SetStateAction<Tech[]>> }) {
   const { setIsIconsShow } = useCreateProject();
+
+  const changeTechName = (value: string) => {
+    setAddedTech(currentTech => {
+      const newTech = currentTech.slice(0);
+
+      newTech[index].name = value;
+
+      return newTech;
+    });
+  }
+
+  const addTechnology = () => {
+
+  }
+
+  const removeTechnology = () => {
+
+  }
 
   return (
     <>
-      
       {/* Added field */}
       <div className="w-full flex flex-wrap gap-y-2 items-center mt-2">
         <input
           type="text"
           placeholder="e.g Front-End"
           className="py-2 px-4 bg-transparent outline-none rounded-lg text-sm border border-primary-800 text-white mt-1 transition duration-300 focus:ring-2 focus:ring-brand-600"
+          onChange={(e) => changeTechName(e.target.value)}
         />
         <CiCirclePlus
           className="text-3xl ms-4 cursor-pointer hover:text-brand-600 text-white"
