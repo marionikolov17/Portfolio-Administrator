0bdde67696d7e5d3b0f3cb40435d0c39d3e9b2a8
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-11-17 18:37:51 +0200

    Create project interface

---
+++ src/entities/projects/interfaces/project.interface.ts
@@ -0,0 +1,11 @@
+export default interface Project {
+    name: string;
+    summary: string;
+    imageUrl: string | File;
+    githubUrl: string;
+    demoUrl: string;
+    images: string[] | File[];
+    features: string[];
+    skills: string[];
+    tech: string[]
+}
